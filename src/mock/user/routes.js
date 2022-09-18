7c9fb4141c008ffebe4ecf5779d688fb9665d8d6
潘老六 <1141195251@qq.com>
2022-09-19 03:51:09 +0800

    init

---
+++ src/mock/user/routes.js
@@ -0,0 +1,30 @@
+// import Mock from 'mockjs'
+//
+// Mock.mock(`${process.env.VUE_APP_API_BASE_URL}/routes`, 'get', () => {
+//   let result = {}
+//   result.code = 0
+//   result.data =  [
+//     {
+//       "router": "root",
+//       "name": "首页",
+//       "icon": null,
+//       "invisible": false,
+//       "page": {
+//         "title": "首页"
+//       },
+//       "children": [
+//         {
+//           "router": "demo",
+//           "name": "演示页",
+//           "icon": "sad",
+//           "invisible": false,
+//           "page": {
+//             "title": "演示页"
+//           },
+//           "children": []
+//         }
+//       ]
+//     }
+//   ]
+//   return result
+// })
---
+++ src/router/async/router.map.js
@@ -0,0 +1,64 @@
+// 视图组件
+const view = {
+  tabs: () => import('@/layouts/tabs'),
+  blank: () => import('@/layouts/BlankView'),
+  page: () => import('@/layouts/PageView')
+}
+
+// 路由组件注册
+const routerMap = {
+  root: {
+    path: '/',
+    name: '首页',
+    redirect: '/login',
+    component: view.tabs
+  },
+  login: {
+    authority: '*',
+    path: '/login',
+    component: () => import('@/pages/login')
+  },
+  auth: {
+    name: '权限',
+    component: view.blank
+  },
+  demo: {
+    name: '演示页',
+    renderMenu: false,
+    component: () => import('@/pages/demo')
+  },
+  user: {
+    name: '用户',
+    component: () => import('@/pages/demo')
+  },
+  menu: {
+    name: '菜单',
+    component: () => import('@/pages/demo')
+  },
+  resource: {
+    name: '资源',
+    component: () => import('@/pages/demo')
+  },
+  exp403: {
+    authority: '*',
+    name: 'exp403',
+    path: '403',
+    component: () => import('@/pages/exception/403')
+  },
+  exp404: {
+    name: 'exp404',
+    path: '404',
+    component: () => import('@/pages/exception/404')
+  },
+  exp500: {
+    name: 'exp500',
+    path: '500',
+    component: () => import('@/pages/exception/500')
+  },
+  exception: {
+    name: '异常页',
+    icon: 'warning',
+    component: view.blank
+  }
+}
+export default routerMap
